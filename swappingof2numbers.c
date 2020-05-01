6d3d8ca1269faa09120e1782b88cdb82f75ba2f3
gagandeepna <64635095+gagandeepna@users.noreply.github.com>
2020-05-01 16:23:40 +0530

    Created swappingof2numbers.c
    C program for Swapping of two numbers without using a temporary variable.

--- swappingof2numbers.c
+++ swappingof2numbers.c
@@ -3,13 +3,12 @@ int main()
 {
    int a=25, b=100;
    printf("Before swapping a=%d, b=%d \n", a, b);
-   a=a+b;
-   b=a-b;
-   a=a-b;
+   a=a+b; //a=a+b=25+100=125
+   b=a-b; //b=a-b=(a+b)-b =a=25
+   a=a-b; //a=a-b=(a+b)-((a+b)-b) =b=100
    printf("After swapping a=%d, b=%d \n", a, b);
    return 0;
 }
 
-   a=a+b; //a=a+b=25+100=125
-   b=a-b; //b=a-b=(a+b)-b =a=25
-   a=a-b; //a=a-b=(a+b)-((a+b)-b) =b=100
+  
+   
